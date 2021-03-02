04a0044876ddf928ca553dd42a5c6bc6005863a3
JP <typ_0@hotmail.com>
2021-03-02 11:58:32 -0500

    Updates

---
+++ precache-manifest.0a22423edf9b9f6a114835d06735126f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef360d85ca0e8fc48d83918e151f66ef",
+    "url": "/reddit/index.html"
+  },
+  {
+    "revision": "67d5569ab41cd2ed31f3",
+    "url": "/reddit/static/css/main.791fefe6.chunk.css"
+  },
+  {
+    "revision": "68f87657890797a85b26",
+    "url": "/reddit/static/js/2.602abaf1.chunk.js"
+  },
+  {
+    "revision": "f09bafd8b2e7fe4d8e763cfbd1fdf23b",
+    "url": "/reddit/static/js/2.602abaf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d5569ab41cd2ed31f3",
+    "url": "/reddit/static/js/main.ae5f2a58.chunk.js"
+  },
+  {
+    "revision": "ad86a21add63b4c36325",
+    "url": "/reddit/static/js/runtime-main.56153aa8.js"
+  }
+]);
